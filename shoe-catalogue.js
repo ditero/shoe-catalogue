1b98c9574128af34b74f8c740f1a27a8f04503e4
ditero <anelesigenu@gmail.com>
2017-04-06 15:12:19 +0200

    Updated version

--- shoe-catalogue.js
+++ shoe-catalogue.js
@@ -5,7 +5,7 @@ var shoes = [
     size  : 5,
     brand : 'Tusoni laceup',
     in_stock : 5,
-    photo: "Tusoni laceup.jpg"
+    photo: "tusoni.jpg"
   },
   {
     color : 'Black',
@@ -13,16 +13,33 @@ var shoes = [
     size  : 6,
     brand : 'Monk Strap',
     in_stock : 3,
-    photo: "monk.jpg"
+    photo: "monk2.jpg"
+  },
+  {
+    color : 'Dark Brown',
+    price : 575.00,
+    size  : 7,
+    brand : 'Sissy Boy',
+    in_stock : 10,
+    photo: "wmnboot.jpg"
+  },
+  {
+    color : 'Pink',
+    price : 875.00,
+    size  : 5,
+    brand : 'Aldo',
+    in_stock : 10,
+    photo: "laceUp.jpg"
   }
+
 ];
-var addBrand =  document.getElementById('addBrand');
-var addSize =  document.getElementById('addSize');
-var addPrize =  document.getElementById('addPrize');
-var addQty =  document.getElementById('addQty');
-var addColor =  document.getElementById('addColor');
-var browseImg =  document.getElementById('browseImg');
-var btnStock =  document.getElementById('submitStock');
+var addBrand =  null;
+var addSize =  null;
+var addPrize =  null;
+var addQty =  null;
+var addColor =  null;
+var browseImg =  null;;
+var addStockBtn =  null;
 var exitStock = null;
 
 var menListColor =  document.getElementById("menListColor");
@@ -32,38 +49,23 @@ var optColor = document.getElementById('listColor');
 var optSize = document.getElementById('listSize');
 var colorTemplate = Handlebars.compile(optColor.innerHTML);
 var sizeTemplate = Handlebars.compile(optSize.innerHTML);
+var optionColor = null;
+var optionSize = null;
 
 var userName = document.getElementById('userName');
 var passCode = document.getElementById('passCode');
 var addmnBtn = document.getElementById('addmnBtn');
+var errorMsg = null;
 var displayMenu = document.querySelector(".displayMenu");
 var addStockStyle = document.querySelector(".addStockStyle");
 var myTemplate = document.getElementById("myTemplate");
 var myTemplateInstance = Handlebars.compile(myTemplate.innerHTML);
-var sbtn = document.getElementById("sbtn");
+var searchBtn = document.getElementById("sbtn");
 
 var stockTemplate = document.querySelector("#addStock");
 var stockTempInstance = Handlebars.compile(stockTemplate.innerHTML);
 
-window.onload = function () {
-    renderOPtions();
-    var allShoes = myTemplateInstance({shoes});
-    displayMenu.innerHTML = allShoes;
-}
 
-function renderOPtions() {
-    for (var i = 0; i < shoes.length; i++) {
-
-        var optionColor = document.createElement('option');
-        var optionSize = document.createElement('option');
-
-        optionColor.innerHTML = colorTemplate(shoes[i]);
-        optionSize.innerHTML = sizeTemplate(shoes[i]);
-
-        menListColor.appendChild(optionColor);
-        menListSize.appendChild(optionSize);
-    }
-}
 
 
 function searchRslts(){
@@ -91,24 +93,74 @@ else {
 };
 
 
-sbtn.addEventListener('click', searchRslts);
+searchBtn.addEventListener('click', searchRslts);
+//Login and Capture new stock
 addmnBtn.addEventListener('click', function(){
+  errorMsg = document.getElementById('errorMsg');
   if (userName.value === 'admin' && passCode.value === 'admin') {
     //displayMenu.innerHTML  = '';
     addStockStyle.innerHTML = stockTempInstance();
     //
+    addBrand =  document.getElementById('addBrand')
+    addSize =  document.getElementById('addSize');
+    addPrice =  document.getElementById('addPrice');
+    addQty =  document.getElementById('addQty');
+    addColor =  document.getElementById('addColor');
+    browseImg =  document.getElementById('browseImg');
     exitStock =  document.getElementById('exitStockInput');
+    addStockBtn = document.getElementById('submitStock');
 
   }
-  exitStock.addEventListener('click', function(e){
-    // for (var i = 0; i < shoes.length; i++) {
-    //    shoes[i].brand = addBrand.value;
-    //    shoes[i].size = addSize.value;
-    //    shoes[i].price = addPrice.value;
-    //    shoes[i].in_stock = addQty.value;
-    //    shoes[i].color = addColor.value;
-    // }
-    console.log('Stock');
+  else{
+    errorMsg.innerHTML = "Wrong password. Try again.";
+  }
+  // Add Stock into the Object.
+  var objSize = shoes.length;
+  addStockBtn.addEventListener('click', function(e){
+      var myImg = new Image();
+      myImg = browseImg.value.replace("C:\\fakepath\\","");
+
+      var map = {};
+      map.color = addColor.value;
+      map.price = addPrice.value;
+      map.size = addSize.value;
+      map.brand = addBrand.value;
+      map.in_stock  = addQty.value;
+      map.photo = myImg;
+      shoes[objSize] = map;
+//Update dropdown List
+      optionColor = document.createElement('option');
+      optionSize = document.createElement('option');
+      optionColor.innerHTML = colorTemplate(shoes[objSize]);
+      optionSize.innerHTML = sizeTemplate(shoes[objSize]);
+      menListColor.appendChild(optionColor);
+      menListSize.appendChild(optionSize);
+//Display the added item
+      var addedShoe = [];
+      addedShoe.push(shoes[objSize]);
+      var allShoes = myTemplateInstance({shoes : addedShoe});
+      displayMenu.innerHTML = allShoes;
+      objSize++;
+    console.log(shoes);
   }, false);
 });
-//jumbotron.addEventListener('click'function(){});
+
+window.onload = function () {
+    renderOPtions();
+    var allShoes = myTemplateInstance({shoes});
+    displayMenu.innerHTML = allShoes;
+}
+
+function renderOPtions() {
+    for (var i = 0; i < shoes.length; i++) {
+
+        optionColor = document.createElement('option');
+        optionSize = document.createElement('option');
+
+        optionColor.innerHTML = colorTemplate(shoes[i]);
+        optionSize.innerHTML = sizeTemplate(shoes[i]);
+
+        menListColor.appendChild(optionColor);
+        menListSize.appendChild(optionSize);
+    }
+}
