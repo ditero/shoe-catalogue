f618a20755a27d9b79de771487cb319c44bb3a74
ditero <anelesigenu@gmail.com>
2017-04-13 13:06:56 +0200

    Update unique list dropdown options

--- shoe-catalogue.js
+++ shoe-catalogue.js
@@ -3,7 +3,7 @@ var shoes = [
     color : 'Brown',
     price : 2300.00,
     size  : 5,
-    brand : 'Tusoni',
+    brand : 'Kurt Geiger',
     in_stock : 5,
     photo: "tusoni.png"
   },
@@ -11,7 +11,7 @@ var shoes = [
     color : 'Black',
     price : 1500.00,
     size  : 6,
-    brand : 'Monk Strap',
+    brand : 'Tusoni',
     in_stock : 3,
     photo: "monk2.png"
   },
@@ -19,7 +19,7 @@ var shoes = [
     color : 'Dark Brown',
     price : 575.00,
     size  : 7,
-    brand : 'Sissy Boy',
+    brand : 'Magnanni',
     in_stock : 10,
     photo: "wmnboot.png"
   },
@@ -27,7 +27,7 @@ var shoes = [
     color : 'Pink',
     price : 875.00,
     size  : 8,
-    brand : 'Aldo',
+    brand : 'Tusoni',
     in_stock : 10,
     photo: "laceUp.png"
   }
@@ -42,8 +42,8 @@ var browseImg =  null;;
 var addStockBtn =  null;
 var exitStock = null;
 
-var menListColor =  document.getElementById("menListColor");
-var menListSize =  document.getElementById("menListSize");
+var menListColor =  document.querySelector(".menListColor");
+var menListSize =  document.querySelector(".menListSize");
 
 var optColor = document.getElementById('listColor');
 var optSize = document.getElementById('listSize');
@@ -51,8 +51,9 @@ var colorTemplate = Handlebars.compile(optColor.innerHTML);
 var sizeTemplate = Handlebars.compile(optSize.innerHTML);
 var optionColor = null;
 var optionSize = null;
-var brands = document.querySelector('.brands');
-var choseBrand = document.getElementsByTagName('button');
+var kgBrand = document.getElementById('kgBrand');
+var tsBrand = document.getElementById('tsBrand');
+var mgnBrand = document.getElementById('mgnBrand');
 
 var login = document.querySelector('.login');
 var userName = document.getElementById('userName');
@@ -125,70 +126,157 @@ addmnBtn.addEventListener('click', function(){
 
   }
   else{
-    errorMsg.innerHTML = "Wrong password. Try again.";
+    errorMsg.innerHTML = "Wrong password. Default user and password is 'admin'";
   }
   exitStock.addEventListener('click', function(e){
     addStockStyle.innerHTML = "";
   });
+
+
   // Add Stock into the Object.
-     addStockBtn.addEventListener('click', function(e){
+     addStockBtn.addEventListener('click', function(){
        var extOption = shoes.length;
       var myImg = new Image();
       myImg = browseImg.value.replace("C:\\fakepath\\","");
 
+      var size = [];
+
       var map = {};
-      map.color = addColor.value;
-      map.price = addPrice.value;
-      map.size = addSize.value;
-      map.brand = addBrand.value;
-      map.in_stock  = addQty.value;
-      map.photo = myImg;
-      shoes.push(map);
-//Update dropdown List
-      optionColor = document.createElement('option');
-      optionSize = document.createElement('option');
-      optionColor.innerHTML = colorTemplate(shoes[extOption]);
-      optionSize.innerHTML = sizeTemplate(shoes[extOption]);
-      menListColor.appendChild(optionColor);
-      menListSize.appendChild(optionSize);
+      var colorValue = addColor.value;
+      var priceValue = addPrice.value;
+      var sizeValue = addSize.value;
+      var brandValue = addBrand.value;
+      var in_stockValue  = addQty.value;
+      var photoValue = myImg;
 
-//Display the added item to the current list.
-      var allShoes = myTemplateInstance({shoes : shoes});
-      displayMenu.innerHTML = allShoes;
-
-    // console.log(shoes);
+      if(colorValue !== "" && priceValue !== "" && sizeValue !== "" && brandValue !== "" && in_stockValue !== ""&&
+photoValue !== ""){
+        shoes.push({color: colorValue,
+              price: priceValue,
+              size: sizeValue,
+              brand: brandValue,
+              in_stock: in_stockValue,
+              photo: photoValue});
+      }
+      showShoes(shoes);
   }, false);
 });
 
-window.onload = function () {
-    renderOPtions();
-    var allShoes = myTemplateInstance({shoes});
-    displayMenu.innerHTML = allShoes;
-}
+//Create unique color list dropdown values.
+function uniqColorList(shoeList){
+  var shoeColors = [];
+  var mapColor = {};
+  for (var y = 0; y < shoeList.length; y++) {
+    var colorData = shoeList[y];
+    if (mapColor[colorData.color]  === undefined) {
+      mapColor[colorData.color] = colorData.color;
+      shoeColors.push(colorData.color);
 
-function renderOPtions() {
-    for (var i = 0; i < shoes.length; i++) {
-
-        optionColor = document.createElement('option');
-        optionSize = document.createElement('option');
-
-        optionColor.innerHTML = colorTemplate(shoes[i]);
-        optionSize.innerHTML = sizeTemplate(shoes[i]);
-
-        menListColor.appendChild(optionColor);
-        menListSize.appendChild(optionSize);
     }
+  }
+  return shoeColors;
+};
+//Create unique size list dropdown values.
+function uniqSizeList(shoeList){
+  var shoeSizes = [];
+  var mapSizes = {};
+  for (var y = 0; y < shoeList.length; y++) {
+    var sizeData = shoeList[y];
+    if (mapSizes[sizeData.size]  === undefined) {
+      mapSizes[sizeData.size] = sizeData.size;
+      shoeSizes.push(sizeData.size);
+
+    }
+  }
+  return shoeSizes;
+};
+
+
+window.onload = function () {
+    showShoes(shoes);
+
 }
-function tusoni(){
+
+function showShoes(data) {
+ var thisColor = uniqColorList(shoes);
+ var thisSize = uniqSizeList(shoes);
+
+      menListColor.innerHTML = colorTemplate({shoes:thisColor});
+      menListSize.innerHTML = colorTemplate({shoes:thisSize});
+      var allShoes = myTemplateInstance({shoes: shoes});
+      displayMenu.innerHTML = allShoes;
+
+}
+
+function getBrand(){
     var myBrand = [];
     for(var t = 0; t < shoes.length; t++){
-        if(shoes[t].brand === choseBrand.value){
-          myBrand.push(shoes[i]);
+        if(shoes[t].brand === kgBrand.value){
+          myBrand.push(shoes[t]);
+    }
+    else if (shoes[t].brand === mgnBrand.value) {
+      myBrand.push(shoes[t]);
 
     }
+    // else if (shoes[t].brand === tsBrand.value) {
+    //   myBrand.push(shoes[t]);
+    // }
 
     }
      console.log("Tusoni Shoes");
     var showMyBrand = myTemplateInstance({shoes : myBrand});
     displayMenu.innerHTML = showMyBrand;
 }
+var myBrand = [];
+var showMyBrand = null;
+var countBrands = null;
+kgBrand.addEventListener('click', function(){
+  countBrands = 0;
+  displayMenu.innerHTML = "";
+      for(var t = 0; t < shoes.length; t++){
+
+          if(shoes[t].brand === kgBrand.value){
+            if (countBrands === 0) {
+                myBrand.push(shoes[t]);
+            }
+
+      }
+    }
+    showMyBrand = myTemplateInstance({shoes : myBrand});
+    displayMenu.innerHTML = showMyBrand;
+    countBrands++;
+});
+
+tsBrand.addEventListener('click', function(){
+  countBrands = 0;
+  displayMenu.innerHTML = "";
+      for(var t = 0; t < shoes.length; t++){
+
+          if(shoes[t].brand === tsBrand.value){
+            if (countBrands === 0) {
+                myBrand.push(shoes[t]);
+            }
+
+      }
+    }
+    showMyBrand = myTemplateInstance({shoes : myBrand});
+    displayMenu.innerHTML = showMyBrand;
+    countBrands++;
+});
+
+mgnBrand.addEventListener('click', function(){
+  countBrands = 0;
+  displayMenu.innerHTML = "";
+      for(var t = 0; t < shoes.length; t++){
+
+          if(shoes[t].brand === mgnBrand.value){
+            if (countBrands === 0) {
+                myBrand.push(shoes[t]);
+            }
+
+      }
+    }
+    showMyBrand = myTemplateInstance({shoes : myBrand});
+    displayMenu.innerHTML = showMyBrand;
+    countBrands++;
+});
